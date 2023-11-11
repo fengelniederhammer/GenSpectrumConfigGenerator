d6a088d1da8d724269f656a16f4ec1b85470b413
Jonas Kellerer <jonas.kellerer@tngtech.com>
2023-11-11 11:17:23 +0100

    allow to set new config on first button click

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
-import { Button } from '@mui/material';
-import { ConfigProvider } from './configContext.tsx';
-import { useState } from 'react';
-import { ConfigWizard } from './ConfigWizard.tsx';
+import {Button} from '@mui/material';
+import {ConfigProvider} from './configContext.tsx';
+import {useState} from 'react';
+import {ConfigWizard} from './ConfigWizard.tsx';
 
 function App() {
     const [newConfig, setNewConfig] = useState(true);
+    const [config, setConfig] = useState({});
 
     if (newConfig) {
         return (
             <>
-                <h1>Hallo</h1>
-                <Button>New</Button>
-                <Button>Upload</Button>
+                <h1>Config Generator</h1>
+                <Button onClick={() => {
+                    setNewConfig(false);
+                    setConfig({name: 'test'});
+                }}>
+                    New
+                </Button>
+                <Button onClick={() => {
+                    setNewConfig(false);
+                    setConfig({name: 'test2'});
+                }}>
+                    Upload
+                </Button>
             </>
         );
     }
 
     return (
-        <ConfigProvider>
-            <ConfigWizard />
+        <ConfigProvider initialConfig={config}>
+            <ConfigWizard/>
         </ConfigProvider>
     );
 }
