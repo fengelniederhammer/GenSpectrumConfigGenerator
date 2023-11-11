74e45cceeac0dfe4d9b589715b74ee074e57074c
Fabian Engelniederhammer <fabian.engelniederhammer@tngtech.com>
2023-11-11 14:08:45 +0100

    feat: upload config files

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,45 @@
-import {Box, Button} from '@mui/material';
-import {Config, ConfigProvider} from './configContext.tsx';
-import {useState} from 'react';
-import {ConfigWizard} from './ConfigWizard.tsx';
-import {Results} from "./Results.tsx";
+import { Box, Button } from '@mui/material';
+import { Config, ConfigProvider } from './configContext.tsx';
+import { Dispatch, SetStateAction, useState } from 'react';
+import { ConfigWizard } from './ConfigWizard.tsx';
+import { Results } from './Results.tsx';
+import { UploadConfig } from './UploadConfig.tsx';
+import { load } from 'js-yaml';
 
-function App() {
-    const [newConfig, setNewConfig] = useState(true);
-    const [config, setConfig] = useState<Config>({metadata: []});
+export default function App() {
+    const [config, setConfig] = useState<undefined | Config>(undefined);
 
-    if (newConfig) {
+    if (config === undefined) {
         return (
-            <>
+            <Box style={{ textAlign: 'center' }}>
                 <h1>Config Generator</h1>
-                <Button onClick={() => {
-                    setNewConfig(false);
-                    setConfig({
-                        metadata: [
-                            {name: 'fromNew', type: 'string', generateIndex: true},
-                        ]
-                    });
-                }}>
+                <Button onClick={() => setConfig({ metadata: [] })} variant='contained'>
                     New
                 </Button>
-                <Button onClick={() => {
-                    setNewConfig(false);
-                    setConfig({
-                        metadata: [
-                            {name: 'fromUpload', type: 'string', generateIndex: true},
-                        ]
-                    });
-                }}>
-                    Upload
-                </Button>
-            </>
+                <UploadConfig onUpload={(file) => readAndSetConfigFile(file, setConfig)} />
+            </Box>
         );
     }
 
     return (
-        <ConfigProvider initialConfig={config}>#
-            <Box sx={{display: 'flex'}}>
-                <ConfigWizard/>
-                <Results/>
+        <ConfigProvider initialConfig={config}>
+            <Box sx={{ display: 'flex' }}>
+                <ConfigWizard />
+                <Results />
             </Box>
         </ConfigProvider>
     );
 }
 
-export default App;
+function readAndSetConfigFile(file: File, setConfig: Dispatch<SetStateAction<Config | undefined>>) {
+    const fileReader = new FileReader();
+    fileReader.onloadend = () => {
+        if (!fileReader.result) {
+            return;
+        }
+        const fileContent = fileReader.result.toString();
+        const config = load(fileContent);
+        setConfig(config as Config);
+    };
+    fileReader.readAsText(file);
+}
