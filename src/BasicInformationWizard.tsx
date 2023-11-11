53830d95fbbcc83eaed86cc1eabc83c163693465
Fabian Engelniederhammer <fabian.engelniederhammer@tngtech.com>
2023-11-11 11:48:26 +0100

    feat: basic information page layout

--- src/BasicInformationWizard.tsx
+++ src/BasicInformationWizard.tsx
@@ -1,3 +1,41 @@
+import {
+    Box,
+    Checkbox,
+    FormControlLabel,
+    FormGroup,
+    FormLabel,
+    MenuItem,
+    Select,
+    Stack,
+    Switch,
+    TextField,
+    Typography,
+} from '@mui/material';
+import { LAPIS_OPENNESS_OPEN, LAPIS_OPENNESS_PROTECTED } from './configContext.tsx';
+
+const sarsCoV2VariantQuery = 'sarsCoV2VariantQuery';
+
 export function BasicInformationWizard() {
-    return <h1>Basic Information</h1>;
+    return (
+        <>
+            <h1>Basic Information</h1>
+            <TextField label='Instance Name' variant='outlined' />
+            <Stack direction='row' spacing={1} alignItems='center'>
+                <Typography>SILO/LAPIS</Typography>
+                <Switch />
+                <Typography>Pathoplexus</Typography>
+            </Stack>
+            <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+                <Typography>LAPIS Openness</Typography>
+                <Select defaultValue={LAPIS_OPENNESS_OPEN}>
+                    <MenuItem value={LAPIS_OPENNESS_OPEN}>Open</MenuItem>
+                    <MenuItem value={LAPIS_OPENNESS_PROTECTED}>Protected</MenuItem>
+                </Select>
+            </Box>
+            <FormGroup>
+                <FormLabel>LAPIS Features</FormLabel>
+                <FormControlLabel control={<Checkbox />} label='SARS CoV2 Variant Query' />
+            </FormGroup>
+        </>
+    );
 }
--- src/configContext.tsx
+++ src/configContext.tsx
@@ -1,11 +1,16 @@
-import {createContext, FC, PropsWithChildren} from 'react';
+import { createContext, FC, PropsWithChildren } from 'react';
+
+export const LAPIS_OPENNESS_OPEN = 'OPEN';
+export const LAPIS_OPENNESS_PROTECTED = 'PROTECTED';
 
 export type Config = {};
 
 export const ConfigContext = createContext<Config>({});
 
-export const ConfigProvider: FC<PropsWithChildren<{
-    initialConfig: Config
-}>> = ({children, initialConfig}) => {
+export const ConfigProvider: FC<
+    PropsWithChildren<{
+        initialConfig: Config;
+    }>
+> = ({ children, initialConfig }) => {
     return <ConfigContext.Provider value={initialConfig}>{children}</ConfigContext.Provider>;
 };
