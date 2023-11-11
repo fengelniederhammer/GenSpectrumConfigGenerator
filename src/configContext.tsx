185ac75061bc3caf762d02c8d5202ba8ee3ce928
Tobias Kampmann <tobias.kampmann@tngtech.com>
2023-11-11 13:58:23 +0100

    add setter for root Field

--- src/configContext.tsx
+++ src/configContext.tsx
@@ -1,46 +1,52 @@
-import {createContext, FC, PropsWithChildren, useState} from 'react';
+import { createContext, FC, PropsWithChildren, useState } from 'react';
 
 export const LAPIS_OPENNESS_OPEN = 'OPEN';
 export const LAPIS_OPENNESS_PROTECTED = 'PROTECTED';
 
 export type Config = {
-    metadata: Metadata[]
+    metadata: Metadata[];
+    primaryKey: string;
 };
 
 export type ConfigContextType = {
-    config: Config,
-    addMetadata: (metadata: Metadata) => void
-}
+    config: Config;
+    addMetadata: (metadata: Metadata) => void;
+    modifyConfigField: <T extends keyof Config>(field: T, value: Config[T]) => void;
+};
 
-export type MetadataType = 'string' | 'date'
+export type MetadataType = 'string' | 'date';
 
 export type Metadata = {
-    name: string,
-    type: MetadataType,
-    generateIndex: boolean
-}
+    name: string;
+    type: MetadataType;
+    generateIndex: boolean;
+};
 
 export const ConfigContext = createContext<ConfigContextType>({
-    config: {metadata: []},
-    addMetadata: () => {
-    }
-})
+    config: { metadata: [], primaryKey: '' },
+    addMetadata: () => {},
+    modifyConfigField: () => {},
+});
 
+export const ConfigProvider: FC<
+    PropsWithChildren<{
+        initialConfig: Config;
+    }>
+> = ({ children, initialConfig }) => {
+    const [config, setConfig] = useState(initialConfig);
 
-export const ConfigProvider: FC<PropsWithChildren<{
-    initialConfig: Config
-}>> = ({children, initialConfig}) => {
-    const [config, setConfig] = useState(initialConfig)
-    
     const addMetadata = (metadata: Metadata) => {
         setConfig({
             ...config,
-            metadata: [
-                ...config.metadata,
-                metadata
-            ]
-        })
-    }
+            metadata: [...config.metadata, metadata],
+        });
+    };
 
-    return <ConfigContext.Provider value={{config, addMetadata}}>{children}</ConfigContext.Provider>;
+    const modifyConfigField = <T extends keyof Config>(field: T, value: Config[T]) => {
+        setConfig({ ...config, [field]: value });
+    };
+
+    return (
+        <ConfigContext.Provider value={{ config, addMetadata, modifyConfigField }}>{children}</ConfigContext.Provider>
+    );
 };
