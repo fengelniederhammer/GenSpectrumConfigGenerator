32575eaa282d7dc3b6627ae5efdd10616ad45e96
Fabian Engelniederhammer <fabian.engelniederhammer@tngtech.com>
2023-11-11 15:24:35 +0100

    feat: write initial config data to context

--- src/BasicInformationWizard.tsx
+++ src/BasicInformationWizard.tsx
@@ -1,7 +1,6 @@
 import {
     Box,
     Checkbox,
-    FormControlLabel,
     FormGroup,
     FormLabel,
     MenuItem,
@@ -11,15 +10,40 @@ import {
     TextField,
     Typography,
 } from '@mui/material';
-import { LAPIS_OPENNESS_OPEN, LAPIS_OPENNESS_PROTECTED } from './configContext.tsx';
+import {
+    ConfigContext,
+    LAPIS_OPENNESS_OPEN,
+    LAPIS_OPENNESS_PROTECTED,
+    OpennessLevel,
+    PartialConfig,
+} from './configContext.tsx';
+import { useContext, useEffect } from 'react';
 
-const sarsCoV2VariantQuery = 'sarsCoV2VariantQuery';
+const features = [
+    {
+        featureName: 'sarsCoV2VariantQuery',
+        label: 'SARS CoV2 Variant Query',
+    },
+];
 
 export function BasicInformationWizard() {
+    const { config, modifyConfigField } = useContext(ConfigContext);
+
+    useEffect(() => {
+        if (config.opennessLevel === undefined) {
+            modifyConfigField('opennessLevel', LAPIS_OPENNESS_OPEN);
+        }
+    }, [config.opennessLevel, modifyConfigField]);
+
     return (
         <>
             <h1>Basic Information</h1>
-            <TextField label='Instance Name' variant='outlined' />
+            <TextField
+                label='Instance Name'
+                variant='outlined'
+                onChange={(event) => modifyConfigField('instanceName', event.target.value)}
+                defaultValue={config.instanceName ?? ''}
+            />
             <Stack direction='row' spacing={1} alignItems='center'>
                 <Typography>SILO/LAPIS</Typography>
                 <Switch />
@@ -27,15 +51,39 @@ export function BasicInformationWizard() {
             </Stack>
             <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
                 <Typography>LAPIS Openness</Typography>
-                <Select defaultValue={LAPIS_OPENNESS_OPEN}>
+                <Select
+                    defaultValue={config.opennessLevel ?? LAPIS_OPENNESS_OPEN}
+                    onChange={(event) => modifyConfigField('opennessLevel', event.target.value as OpennessLevel)}
+                >
                     <MenuItem value={LAPIS_OPENNESS_OPEN}>Open</MenuItem>
                     <MenuItem value={LAPIS_OPENNESS_PROTECTED}>Protected</MenuItem>
                 </Select>
             </Box>
-            <FormGroup>
-                <FormLabel>LAPIS Features</FormLabel>
-                <FormControlLabel control={<Checkbox />} label='SARS CoV2 Variant Query' />
-            </FormGroup>
+            <FeaturesModifier />
         </>
     );
+}
+
+function FeaturesModifier() {
+    const { config, modifyFeatureFields } = useContext(ConfigContext);
+
+    return (
+        <FormGroup>
+            <FormLabel>LAPIS Features</FormLabel>
+            {features.map(({ featureName, label }) => (
+                <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+                    <FormLabel>{label}</FormLabel>
+                    <Checkbox
+                        key={featureName}
+                        onChange={(event) => modifyFeatureFields(featureName, event.target.checked ? 'add' : 'delete')}
+                        defaultChecked={hasFeature(config, featureName)}
+                    />
+                </Box>
+            ))}
+        </FormGroup>
+    );
+}
+
+function hasFeature(config: PartialConfig, featureName: string) {
+    return config.features?.some((feature) => feature.name === featureName) ?? false;
 }
